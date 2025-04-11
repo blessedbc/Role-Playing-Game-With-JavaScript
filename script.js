d4749896bb3b1d950b8d15864c74fecff2e18e50
ikenna nwaka <inwaka39@gmail.com>
2025-04-11 19:10:25 +0200

    goldText

--- script.js
+++ script.js
@@ -11,3 +11,4 @@ const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
