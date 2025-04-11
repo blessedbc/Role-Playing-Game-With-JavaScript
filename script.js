b2278d9d4d119d0c02ab3db70739e74e3b76c570
ikenna nwaka <inwaka39@gmail.com>
2025-04-11 19:09:35 +0200

    healthText

--- script.js
+++ script.js
@@ -10,3 +10,4 @@ const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
 const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
