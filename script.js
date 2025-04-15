b40e27eec36235054ee9c254a3a348e0aa8f6c31
ikenna nwaka <inwaka39@gmail.com>
2025-04-15 19:08:58 +0200

    getMonsterAttackValue(level)

--- script.js
+++ script.js
@@ -208,3 +208,9 @@ function attack() {
     currentWeapon--;
   }
 }
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  console.log(hit);
+  return hit > 0 ? hit : 0;
+}
